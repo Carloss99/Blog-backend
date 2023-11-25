3419440e373af2e2475673106434512aa834683d
carlos6199 <carlos_fig14@hotmail.com>
2023-11-24 22:09:42 -0500

    .

---
+++ blogapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import Blog
+from rest_framework import viewsets, permissions
+from .serializers import BlogSerializer
+
+# Create your views here.
+
+class BlogViewSet(viewsets.ModelViewSet):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+    permission_class = [permissions.AllowAny]
